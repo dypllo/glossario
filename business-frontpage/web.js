7b94d4afe871d8c3f1d0a4d458001cc55111baf8
Anastacia Drebot <dypllo@gmail.com>
2013-10-28 14:40:45 +0000

    Update

--- business-frontpage/web.js
+++ business-frontpage/web.js
@@ -5,10 +5,16 @@ var htmlfile = "index.html";
 
 
 
+
 app.get('/', function(request, response) {
   var html = fs.readFileSync(htmlfile).toString();
 response.send(html);
 });
+
+
+app.use(express.static(__dirname + '/business-frontpage'));
+
+
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
